be32a4d02b64f12e4a11618279f7ad27dd6c0563
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2022-04-07 00:10:15 +0200

    LinkedList_ Constructor

---
+++ Linked List/LinkedList_ Constructor.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+
+mylist = LinkedList(7)
+
+print(mylist.head.value)
+
+print(mylist.tail.value)
